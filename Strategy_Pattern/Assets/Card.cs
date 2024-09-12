f9d31d28ebc7a0ede3ac6455376bac1f7298a6ab
owen22s <34884@ma-web.nl>
2024-09-12 12:07:58 +0100

    added Basic card class

---
+++ Strategy_Pattern/Assets/Card.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public float card_cost;
+    public float card_time;
+    public float card_rate;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
